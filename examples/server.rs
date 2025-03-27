43952fe5999bdad5c4385b6e4442946f4b7d8e2f
andylokandy <andylokandy@hotmail.com>
2025-03-27 19:23:53 +0800

    chore: add server example

--- examples/server.rs
+++ examples/server.rs
@@ -3,20 +3,18 @@ use fastrace::collector::ConsoleReporter;
 
 #[tokio::main]
 async fn main() {
-    // Configurate fastrace reporter.
+    // Initialize fastrace with the console reporter.
     fastrace::set_reporter(ConsoleReporter, Config::default());
 
     let app = axum::Router::new()
         .route("/ping", axum::routing::get(ping))
-        // Add a the FastraceLayer to routes.
-        // The layer extracts trace context from incoming requests.
         .layer(fastrace_axum::FastraceLayer);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-#[fastrace::trace] // Trace individual handlers.
+#[fastrace::trace]
 async fn ping() -> &'static str {
     "pong"
 }
